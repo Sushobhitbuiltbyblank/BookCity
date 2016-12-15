3a1157fff370cc46ddf959650026a0210a16e9af
Sushobhit jain <sushobhit.builtbyblank@gmail.com>
2016-12-15 19:09:57 +0530

    working on shopdetailview working hours done in this view

---
+++ BookCities/State+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  State+CoreDataClass.swift
+//  
+//
+//  Created by Sushobhit_BuiltByBlank on 12/13/16.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(State)
+public class State: NSManagedObject {
+
+}
---
+++ State+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  State+CoreDataProperties.swift
+//  
+//
+//  Created by Sushobhit_BuiltByBlank on 12/13/16.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension State {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<State> {
+        return NSFetchRequest<State>(entityName: "State");
+    }
+
+    @NSManaged public var country_id: String?
+    @NSManaged public var id: Int64
+    @NSManaged public var name: String?
+
+}
